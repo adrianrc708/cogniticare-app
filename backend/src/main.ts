0008be4d1787126b6d9adfe3272800c9489003fa
Adrian <adrian.rivera@unmsm.edu.pe>
2025-11-20 20:48:41 -0500

    3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,20 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+    app.enableCors({
+        // CAMBIA AQUÍ el origen para que coincida con la dirección actual del frontend
+        origin: process.env.FRONTEND_URL || 'http://127.0.0.1:5174',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  const port = process.env.BACKEND_PORT || 3000;
-  
-  await app.listen(port);
-  console.log(`\n🚀 Servidor NestJS escuchando en http://localhost:${port}`);
+    const port = process.env.BACKEND_PORT || 3000;
+
+    await app.listen(port);
+    console.log(`\n🚀 Servidor NestJS escuchando en http://localhost:${port}`);
 }
 
 bootstrap();
